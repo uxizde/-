186d50624db4656d0b51eb635b2d9609ae4a6708
Chester Holtz <chesterholtz@yahoo.com>
2014-11-15 16:50:35 -0500

    Boid Class

---
+++ SwarmDataVisualization/Assets/scripts/Boid.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+	public int state = 1;
+	public Vector3 position { get; set; }
+	public Vector3 velocity { get; set; }
+}
